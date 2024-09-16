fc49128307fe28aba4174434c0ddb947705aaa25
Phong <nhannguyenqna2002@gmail.com>
2024-09-16 21:59:28 +0700

    easy to CRUD book

---
+++ book/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from book.views import BookListCreate, BookRetrieveUpdateDestroy
+
+urlpatterns = [
+    path('books/', BookListCreate.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroy.as_view(), name='book-retrieve-update-destroy'),
+]
--- book/views.py
+++ book/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+from book.models import Book
+from book.serializers import BookSerializer
+
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def delete(self, request, *args, **kwargs):
+        book = self.get_object()
+        book.delete()
+        return Response(status=204)
+
